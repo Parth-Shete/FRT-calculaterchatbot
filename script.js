6cd4d7f5adb5dade9b64a99ffce6d57391f9fa63
Parth Shete <73226958+Parth-Shete@users.noreply.github.com>
2022-10-21 21:41:06 +0530

    Update and rename index.js to script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+body {background-color:black;
+	color:white;
+	font:arial;
+}
+
+h1 {color:grey;
+	font:verdana;
+	padding: 0px 10px;
+}
+
+.maindiv { background-color:white;
+	color:black;
+	width:400px;
+	padding:20px;
+	border-radius:25px;
+	margin:auto;
+	line-height:normal;
+}
+
+#mainform { margin:auto; 
+	line-height:30px;
+	display:table;
+}
+
+#mainform p { display: table-row; 
+}
+
+#mainform label { display:table-cell
+}
+
+.result { display:none;
+}
+
+footer {padding: 0px 10px;
+	padding-top:10px;
+}
